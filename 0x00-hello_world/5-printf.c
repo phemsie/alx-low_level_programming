f03c1d4b45ca492203e95caa141a717de0580b4b
phemsie <femi_oworu@yahoo.com>
2022-09-08 07:03:18 -0700

    Task 5

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,22 @@
+#!/bin/bash
+#include <stdio.h>
+
+/**
+ *
+ *    * main - Prints onto screen
+ *
+ *       *
+ *
+ *          * Return: zero
+ *
+ *             */
+
+int main(void)
+
+{
+
+		printf("with proper grammar, but the outcome is a piece of art,\n");
+
+			return (0);
+
+}
