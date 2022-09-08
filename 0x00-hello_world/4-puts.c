1e8ca2474a62a3cb249386801810db4e4af17293
phemsie <femi_oworu@yahoo.com>
2022-09-08 07:56:10 -0700

    Task 4

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -2,20 +2,20 @@
 #include <stdio.h>
 /**
  *
- *  * main - Entry point
+ * main - Entry point
  *
- *   *
  *
- *    * Return: Always 0 (Success)
  *
- *     */
+ * Return: Always 0 (Success)
+ *
+ */
 
 int main(void)
 
 {
 
-	        puts("\"Programming is like building a multilingual puzzle");
+	puts("\"Programming is like building a multilingual puzzle");
 
-		        return (0);
+		 return (0);
 
 }
