3d18ac693b5ffd6c27b6499420daae6d221d47e5
phemsie <femi_oworu@yahoo.com>
2022-09-08 06:54:53 -0700

    Task 4

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,8 +1,19 @@
 #!/bin/bash
+#include <stdio.h>
+/**
+ *
+ *    * main - print to screen
+ *
+ *       *
+ *
+ *          * Return: zero
+ *
+ *             */
+
 int main(void)
 
 {
 
 		char Mystr[] = "\"Programming is like building a multilingual puzzle";
-
-
+		return (0)
+}
