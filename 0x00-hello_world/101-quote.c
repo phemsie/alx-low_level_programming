8f293a20bfc62d22c6378e3663059072c9717ebf
phemsie <femi_oworu@yahoo.com>
2022-09-08 17:59:12 -0700

    Task 8

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,12 +1,13 @@
-#include <unistd.h>
-
-/**
- *main - print
- *
- * Return: Always 1 (successful)
- */
-int main(void)
-{
-	write(2, "and that piece of art is useful \" - Dora Korpar, 2015-10-19\n", 59);
-	return (1);
+#include <stdio.h>                                                            
+#include <unistd.h>                                                           
+/**                                                                           
+ * main - entry point                                                         
+ *                                                                            
+ * Description: prints and that piece..                                       
+ * Return: 1 if sucessful                                                      
+ */                                                                           
+int main(void)                                                                
+{                                                                             
+	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);                                                        
+	return (1);                                                           
 }
