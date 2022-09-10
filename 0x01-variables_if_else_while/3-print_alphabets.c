5a75f766e50467c5fbad7e0055768c789284f9b8
phemsie <femi_oworu@yahoo.com>
2022-09-09 19:15:22 -0700

    Task 3

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -12,9 +12,9 @@ int main(void)
 	char (ch)
 	char (CH)
 	for (ch = 'a' ; ch <= 'z' ; ch++)
-		putchar (ch)
+		putchar (ch);
 	for (CH = 'A' ; CH <= 'Z' ; CH++)
-		putchar (CH)
+		putchar (CH);
 		putchar ('\n');
 	return (0)
 }
