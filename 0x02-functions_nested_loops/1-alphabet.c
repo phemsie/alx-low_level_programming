3ae35f83ac45cf0fd33a0c0dfc6798d7e3461b71
phemsie <femi_oworu@yahoo.com>
2022-09-14 09:49:28 -0700

    Task 1

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,12 +1,13 @@
 #include "main.h"
 /**
- * Print alphabet - function to print abc
- *
+ * Print alphabet - function to print abc*
  * Return: 0
  */
-void print_alphabet(void)
+void print_alphabet(void);
 {
 	char c;
+lphabet.c
+
 
 	for (c = 'a'; c <= 'z'; c++)
 	{
