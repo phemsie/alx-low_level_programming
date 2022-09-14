f23aa2f8adc62ec3c1c5a54b86e33d0e05cfa043
phemsie <femi_oworu@yahoo.com>
2022-09-14 07:39:42 -0700

    Task 0

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,5 +1,4 @@
-#include <main.h>
-#include <stdio.h>
+#include "main.h"
 
 /**
  * main - print _putchar
@@ -9,14 +8,16 @@
 int main(void)
 {
 	_putchar('_');
-	_putchar('P');
+	_putchar('p');
 	_putchar('u');
 	_putchar('t');
 	_putchar('c');
 	_putchar('h');
 	_putchar('a');
 	_putchar('r');
-	_putcahar('\n');
+	_putchar('\n');
 
 	return (0);
+
 }
+
