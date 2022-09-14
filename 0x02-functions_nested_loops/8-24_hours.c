ef31080cea285f5762365095ef01fe2923018816
phemsie <femi_oworu@yahoo.com>
2022-09-14 16:13:27 -0700

    Task 8

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -10,23 +10,23 @@ void jack_bauer(void)
 	int hours = 0;
 	int minutes = 0;
 	int hours_remainder = 0;
-	int minutes_remainder = 0;
+	int mins_remainder = 0;
 
 	while (hours <= 23)
 	{
 	while (minutes <= 59)
 	{
-	minutes_remaimder = minutes % 10;
+	mins_remaimder = minutes % 10;
 	hours_remainder = hours % 10;
 	_putchar(hours / 10 + '0');
 	_putchar(hours_remainder + '0');
 	_putchar(':');
 	_putchar(minutes / 10 + '0');
-	_putchar(minutes_remainder + '0');
+	_putchar(mins_remainder + '0');
 	minutes++;
 	_putchar('\n');
 	}
-	hours++
+	hours++;
 	minutes = 0;
 	}
 }
