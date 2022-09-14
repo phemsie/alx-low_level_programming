064ccc49c14688071d25e3091ae0fa63f188a45a
phemsie <femi_oworu@yahoo.com>
2022-09-14 14:07:24 -0700

    Task 6

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * _abs - function that computes the absolute vakue of an integer
+ * @c: is the int that we use for the argument of the function
+ * Return: 0
+ */
+int _abs(int c)
+{
+	if (c > 0 || c == 0)
+	{
+	return (c);
+	}
+	else
+	return (c * -1);
+}
