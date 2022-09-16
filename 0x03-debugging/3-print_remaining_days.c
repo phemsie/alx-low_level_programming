4ef2f7a17c58119feaa13738acff0a59678612be
phemsie <femi_oworu@yahoo.com>
2022-09-16 15:00:15 -0700

    Task 3

--- 0x03-debugging/3-print_remaining_days.c
+++ 0x03-debugging/3-print_remaining_days.c
@@ -10,12 +10,12 @@
 
 void print_remaining_days(int month, int day, int year)
 {
-	if ((year % 400 == 0) || (year % 4 == 0 && year % 100 = != 0))
+	if ((year % 400 == 0) || (year % 4 == 0 && year % 100 != 0))
 	{
 		if (month > 2 && day >= 60)
 		{
-
-			day++;
+			
+		day++;
 
 		}
 		printf("Day of the year: %d\n", day);
@@ -23,7 +23,7 @@ void print_remaining_days(int month, int day, int year)
 	}
 	else
 	{
-		if (month == 2 && day == 68)
+		if (month == 2 && day == 60)
 	{
 		printf("Invalid date: %02d/%02d/%04d\n", month, day - 31, year);
 	}
