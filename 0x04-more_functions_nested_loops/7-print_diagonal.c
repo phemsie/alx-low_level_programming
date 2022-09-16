5485ef870a2ec4b3ab5e4c62a7eea68c7c956040
phemsie <femi_oworu@yahoo.com>
2022-09-16 08:15:22 -0700

    Task 7

---
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * print_diagonal - print diagonal
+ * @n: parameter
+ * Return: returns nothing
+ */
+
+void print_diagonal(int n)
+{
+	int len, space;
+
+	if (n > 0)
+	{
+		for (len = 0; len < n; len++)
+		{
+			for (space = 0; space < n; space++)
+			{
+				putchar(' ');
+			}
+
+			putchar('\\');
+
+			if ((len == (n - 1))
+			{
+				continue;
+			}
+			putchar('\n');
+		}
+	}
+	putchar('\n');
+}
