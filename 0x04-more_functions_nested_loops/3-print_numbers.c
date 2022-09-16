fa5baa217b526dba127c1f78fa42542e47dda935
phemsie <femi_oworu@yahoo.com>
2022-09-16 07:01:20 -0700

    Task 3

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -1,10 +1,10 @@
 #include "main.h"
-
+#include <stdio.h>
 /**
  * print_numbers - function that print all numbers from
  * 0 to 9
  *
- * Return: return nothing
+ * Return: returns nothing
  */
 
 void print_numbers(void)
