7a3324d3371006f7fb591794b7f0a4e20ef4ec4a
phemsie <femi_oworu@yahoo.com>
2022-09-17 12:30:11 -0700

    Task 12

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -30,11 +30,11 @@ void print_times_table(int n)
 				if (prod >= 100)
 				{
 					_putchar((prod / 100) + 48);
-					_putchar((prod / 10 % 10 + 48);
+					_putchar((prod / 10) % 10 + 48);
 				}
 				else if (prod <= 99 && prod >= 10)
-					_putchar((prod / 10) + 48);
-					_putchar((prod % 10) + 48);
+				_putchar((prod / 10) + 48);
+				_putchar((prod % 10) + 48);
 			}
 			_putchar('\n');
 		}
