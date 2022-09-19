7a3bf27c17b02b88a5168249e38d569b6c8bf721
phemsie <femi_oworu@yahoo.com>
2022-09-19 08:59:26 -0700

    Task 4

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+ * print_rev - print a string in reverse
+ * @s: string to printed in reverse
+ */
+
+void print_rev(char *s)
+{
+	int i, j, len;
+
+	i = 0
+
+	while (s[i] != '\0')
+	{
+		i++;
+	}
+
+	len = i
+
+	for (j = len - 1; j >= 0; j--)
+	{
+		_putchar(s[j]);
+	}
+
+	_putchar('\n');
+}
