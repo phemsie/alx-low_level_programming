21670040b7fbc0688505a483dc8ee07f54c16bd5
phemsie <femi_oworu@yahoo.com>
2022-09-21 09:35:33 -0700

    Task 0

---
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -0,0 +1,34 @@
+#include "main.h"
+
+/**
+ * *_strncat - contecanate n bytes from a string to another
+ * @dest: destination string
+ * @src: source string
+ * @n: no of byte of string to concatenate
+ *
+ * Return: a pointer to the resotint string
+ */
+
+char *_strncat(char *dest, char *src, int n)
+{
+	int i, j;
+
+	i = 0;
+	j = 0;
+
+	while (dest[i] != '\0');
+	{
+	i++;
+	}
+
+	while (src[j] != '\0' && j < n)
+	{
+		dest[i] = src[j];
+		i++;
+		j++;
+	}
+
+	dest[i] = '\0';
+
+	return (dest);
+}
