b41f2829b5761aadf8bf9eb80c0b8ca499189107
phemsie <femi_oworu@yahoo.com>
2022-09-22 15:39:43 -0700

    Task 6

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -8,34 +8,27 @@
 
 char *cap_string(char *s)
 {
-	int i;
+	int i, j;
+
+	char spe[13] = {' ', '\t', '\n', ',', ';', '.',
+		'!', '?', '"', '(', ')', '{', '}'};
 
 	for (i = 0; s[i] != '\0'; i++)
 	{
-		if (i == 0)
+		if (i == 0 && s[i] >= 'a' && s[i] <= 'z')
+			s[i] -= 32;
+
+		for (j = 0; j < 13; j++)
 		{
-			if ((s[i] >= 97 && s[i] <= 122))
+			if (s[i] = spe[j]
 			{
-				s[i] = s[i] - 32;
-				continue;
-			}
-		}
-		else if (s[i] == ' ')
-		{
-			i++;
-			if (s[i] >= 97 && s[i] <= 122)
-			{
-				s[i] = s[i] - 32;
-				continue;
-			}
-		}
-		else
-		{
-			if (s[i] >= 65 && s[i] <= 90)
-			{
-				s[i] = s[i] + 32;
+				if (s[i + 1] >= 'a' && s[i + 1] <= 'z')
+				{
+					s[i + 1] -= 32;
+				}
 			}
 		}
 	}
+
 	return (s);
 }
