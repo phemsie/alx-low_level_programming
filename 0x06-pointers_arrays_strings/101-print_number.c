c567cf66eee3030f3a95c93eff3fc89921066d89
phemsie <femi_oworu@yahoo.com>
2022-09-23 01:05:02 -0700

    Task 9

---
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * print_number - print an integer
+ * @n: integer to be printed
+ */
+
+void print_number(int n)
+{
+	unsigned int n1;
+
+	if (n < 0)
+	{
+		n1 = -n
+			_putchar('-');
+	}
+	else
+	{
+		n1 = n;
+	}
+	if (n / 10)
+		print_number(n / 10);
+
+	_putchar((n % 10) + '0');
+}
