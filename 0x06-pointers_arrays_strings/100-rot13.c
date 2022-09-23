6ac4205290cb23613e0a080855395379ba116dd1
phemsie <femi_oworu@yahoo.com>
2022-09-23 14:12:10 -0700

    Task 8

--- 0x06-pointers_arrays_strings/100-rot13.c
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -14,13 +14,13 @@ char *rot13(char *s)
 	char a[] = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
 	char b[] = "NOPQRSTUVWXYZABCDEFGHIJKLMnopqrstuvwxyzabcdefghijklm";
 
-	for (i = 0; s[i] != '\0'; i++)
+	for (i = 0; (s + i); i++)
 	{
-		for (j = 0; a[j] != '\0'; j++)
+		for (j = 0; j < 52; j++)
 		{
-			if (s[i] == a[j])
+			if (*(s + i) == a[j])
 			{
-				s[i] = b[j];
+				*(s + i) = b[j];
 				break;
 			}
 		}
