a3122ed1c63b2c12f0b19102b8417fbcb574b30b
phemsie <femi_oworu@yahoo.com>
2022-09-23 02:38:29 -0700

    Task 10

--- 0x06-pointers_arrays_strings/102-magic.c
+++ 0x06-pointers_arrays_strings/102-magic.c
@@ -16,7 +16,7 @@ int main(void)
 	 * - only one statement
 	 * - you are not allowed to code anything other than this
 	 */
-	*(a + 2) = 98
+	*(a + 2) = 98;
 	/*... so that this print 98\n */
 	printf("a[2] = %d\n", a[2]);
 	return (0);
