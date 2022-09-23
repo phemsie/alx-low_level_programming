dd1050a3e806bea3472164c390e97e7489046f46
phemsie <femi_oworu@yahoo.com>
2022-09-23 01:46:06 -0700

    Task 9

--- 0x06-pointers_arrays_strings/101-print_number.c
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -19,8 +19,8 @@ void print_number(int n)
 	{
 		n1 = n;
 	}
-	if (n / 10)
-		print_number(n / 10);
+	if (n1 / 10)
+		print_number(n1 / 10);
 
-	_putchar((n % 10) + '0');
+	_putchar((n1 % 10) + '0');
 }
