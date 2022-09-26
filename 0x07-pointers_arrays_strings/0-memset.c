57ffd9af3c097cb837ec21419fe65ad3245e7d8f
phemsie <femi_oworu@yahoo.com>
2022-09-26 07:51:13 -0700

    Task 0

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -11,12 +11,10 @@
 
 char *_memset(char *s, char b, unsigned int n)
 {
-	unsigned int n;
+	unsigned int i;
 
-	while (n)
-	{
-		s[n - 1] = b;
-		n--;
-	}
+	for (i = 0; i < n; i++)
+		s[i] = b;
+
 	return (s);
 }
