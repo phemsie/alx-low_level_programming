3036374b9ce6791562c01c77a9edcea7dc7026af
phemsie <femi_oworu@yahoo.com>
2022-09-26 06:41:09 -0700

    Task 1

--- 0x07-pointers_arrays_strings/1-memcpy.c
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -13,9 +13,9 @@ char *_memcpy(char *dest, char *src, unsigned int n)
 {
 	unsigned int i = 0;
 
-	for (; i < n; i++)
+	for (i; i < n; i++)
 	{
-		*dest(i) = *src(i);
+		dest[i] = src[i];
 	}
 
 	return (dest);
