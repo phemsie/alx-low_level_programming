092b418e6b7fb02964e6164a13a62a193667c944
phemsie <femi_oworu@yahoo.com>
2022-09-28 09:18:47 -0700

    Task 6

--- 0x08-recursion/6-is_prime_number.c
+++ 0x08-recursion/6-is_prime_number.c
@@ -15,3 +15,20 @@ int is_prime_number(int n)
 		return (0);
 	return (is_prime(n, start));
 }
+
+/**
+ * is_prime - return 1 if prime
+ * @n: number to be checked
+ * @start: number to start checking from
+ *
+ * Return:1 if n is prime 0 otherwise
+ */
+
+is_prime(int n, int start)
+{
+	if (start <= 1)
+		return (1);
+	else if (n % start == 0)
+		return (0);
+	return (is_prime(n, start - 1));
+}
