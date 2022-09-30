51e33b4304e1cc6b2933d27db755265c4ccc208f
phemsie <femi_oworu@yahoo.com>
2022-09-30 11:09:02 -0700

    Task 0

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include "main.h"
 
 /**
  * main - print its name follow by a new line
@@ -11,7 +12,7 @@
 int main(int argc, char *argv[])
 {
 	(void) argc;
-	printf("%\n", argv[0]);
+	printf("%s\n", argv[0]);
 
 	return (0);
 }
