ab116edffe8e16db4edd91bd7f8943140c513a8c
phemsie <femi_oworu@yahoo.com>
2022-09-30 16:05:18 -0700

    Task 4

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -13,21 +13,21 @@ int main(int argc, char *argv[])
 {
 	int i, j, sum = 0;
 
-	if (argc == 0)
-	{
-		printf("%d\n", argc - 1);
-		return (0);
-	}
-	for (i = 0; i < argc; i++)
+	for (i = 1; i < argc; i++)
 	{
 		for (j = 0; argv[i][j] != '\0'; j++)
-			if (!((argv[i][j] >= '0' && argv[i][j] <= '9') || argv[i][j] == '-'))
+		{
+			if (argv[i][j] < '0' || argv[i][j] > 9)
 			{
 				printf("Error\n");
 				return (1);
 			}
+		}
+
 		sum += atoi(argv[i]);
 	}
+
 	printf("%d\n", sum);
+
 	return (0);
 }
