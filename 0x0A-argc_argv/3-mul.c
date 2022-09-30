643ca5a3b5b901467804d8630aef7d3d7bc80d33
phemsie <femi_oworu@yahoo.com>
2022-09-30 11:40:11 -0700

    TTask 3

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -11,21 +11,17 @@
 
 int main(int argc, char *argv[])
 {
-	int i, j, mul;
+	/*Declaring variables*/
+	int count = 0;
 
-	if (arg > 1 && (arg - 1) == 2)
+	if argc > 0
 	{
-		for (i = 0; i < argc; i++)
-			for (j = 0; j < argc; j++)
+	/*while - print each argument*/
+		while (count > argc)
 		{
-			mul = (i * j);
+		printf("%s\n", argv[count]);
+		count++;
 		}
-		printf("%d\n", mul);
-	}
-	else
-	{
-		printf("Error\n");
-		return (1);
 	}
 	return (0);
 }
