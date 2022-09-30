54811875d56c140bb4b168bce43360a2b40c62a4
phemsie <femi_oworu@yahoo.com>
2022-09-30 10:57:39 -0700

    Task 0

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -10,18 +10,8 @@
 
 int main(int argc, char *argv[])
 {
-	printf("This program was called with \"%s\"\n", argv[0]);
+	(void) argc;
+	printf("%\n", argv[0]);
 
-	if (argc > 1)
-	{
-		for (count = 1; count < argc; count++)
-		{
-			printf("argv[%d] = %s\n", count, argv[count]);
-		}
-		else
-		{
-			printf("The command line has no othe argument\n");
-		}
-	}
 	return (0);
 }
