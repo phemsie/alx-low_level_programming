b128626dad7f97a50f5ddb98b85ed6dddcf88ab4
phemsie <femi_oworu@yahoo.com>
2022-10-04 14:54:58 -0700

    101-strtow.c

--- 0x0B-malloc_free/101-strtow.c
+++ 0x0B-malloc_free/101-strtow.c
@@ -72,7 +72,8 @@ char **strtow(char *str)
 				size++;
 			else if (((str[i] == ' ') || (str[i] == '\0')) && i && (str[i - 1] != ' '))
 			{
-				split[j] = (char *) malloc(sizeof(char) * size + 1);			if (split[j] != NULL)
+				split[j] = (char *) malloc(sizeof(char) * size + 1);
+				if (split[j] != NULL)
 				{
 					while (temp < size)
 					{
