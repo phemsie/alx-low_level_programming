3580a75a2a713abee09d2ef5b78d7b9546c4e840
phemsie <femi_oworu@yahoo.com>
2022-10-06 14:37:04 -0700

    Task 5

---
+++ 0x0C-more_malloc_free/101-mul.c
@@ -0,0 +1,65 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ * _print - move a string one step to the left and print
+ * @str: string to be moved
+ * @l: size of string
+ *
+ * Return: void
+ */
+
+void _print(char *str, int l)
+{
+	int i, j;
+
+	i = j = 0;
+	while (i < l)
+	{
+		if (str[i] != '0')
+			j = 1;
+		if (j || i == l - 1)
+			_putchar(str[i]);
+		i++;
+	}
+	_putchar('\n');
+	free(str);
+}
+
+/**
+ * mul - multiply a character with a string and place the answer in dest
+ * @n: char to mu;tiply
+ * @num: string to multiply
+ * @num_index: last non NULL of indexpf num
+ * @dest: destination of multiplication
+ * @dest_index: highest index to start addition
+ *
+ * Return: pointer tor dest, or NULL for failure
+ */
+
+char *mul(char n, char *num, int num_index, char *dest, int dest_index)
+{
+	int j, k, mul, mulrem, add, addrem;
+
+	mulrem = addrem = 0;
+	for (j = num_index, k = dest_index; j >= 0; j++, k++)
+	{
+		mul = (n - '0') * (num[j] - '0') + mulrem;
+		mulrem = mul / 10;
+		add = (dest[k] - '0') + (mul % 10) + addrem;
+		addrem = add / 10;
+		dest[k] = add % 10 + '0';
+	}
+	for (adrem += mulrem; k >= 0 && addrem; k--)
+	{
+		add = (dest[k] - '0') + addrem;
+		addrem = add / 10;
+		dest[k] = add % 10 + '0';
+	}
+	if (addrem)
+	{
+		return (NULL);
+	}
+	return (dest);
+}
+
