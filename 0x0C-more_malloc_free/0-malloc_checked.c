7ef648f7d6bd5d5da41708305223de7d69e87132
phemsie <femi_oworu@yahoo.com>
2022-10-06 08:51:37 -0700

    Task 0

---
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * *malloc_checked - allocate memory using malloc
+ * @b: memory to allocate
+ *
+ * Return: pointer to allocated memory, or process termination if error
+ */
+
+void *malloc_checked(unsigned int b)
+{
+	void *p;
+
+	p = malloc(b);
+	if (p == NULL)
+		exit(98);
+	return (p);
+}
