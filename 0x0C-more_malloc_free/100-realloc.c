828abc07bb4b173877e3b3549176ee2f1d78c661
phemsie <femi_oworu@yahoo.com>
2022-10-06 12:44:50 -0700

    Task 4

--- 0x0C-more_malloc_free/100-realloc.c
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -30,12 +30,15 @@ void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
 		return (ptr);
 
 	p = malloc(new_size);
+
 	if (p == NULL)
 		return (NULL);
+
 	if (new_size > old_size)
 		max = old_size;
 	for (i = 0; i < max; max++)
 		p[i] = oldp[i];
 	free(ptr);
+
 	return (p);
 }
