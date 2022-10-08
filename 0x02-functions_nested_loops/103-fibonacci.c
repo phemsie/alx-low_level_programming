ea82ae880900e8c87abeb784c97b98e933a0672d
phemsie <femi_oworu@yahoo.com>
2022-10-08 13:55:56 -0700

    Task 15

--- 0x02-functions_nested_loops/103-fibonacci.c
+++ 0x02-functions_nested_loops/103-fibonacci.c
@@ -1,26 +1,31 @@
-#include "main.h"
 #include <stdio.h>
-#include <stdlib.h>
 
 /**
- * numLength - returns the length of string
- * @num: operand number
- * Return: number of digits
+ * main - finds and prints the sum of the even-valued terms
+ * followed by a new line
+ * Return: Always 0 (Success)
  */
 
-int numLength(int num)
+int main(void)
 {
-	int Length = 0;
+	int i;
+	unsigned long int j, k, next, sum;
 
-	if (!num)
-	{
-	return (1);
-	}
-	while (num)
-	{
-		num = num / 10;
-		Length += 1;
-	}
+	j = 1;
+	k = 2;
+	sum = 0;
 
-	return (Length);
+	for (i = 1; i <= 33; ++i)
+	{
+		if (j < 4000000 && (j % 2) == 0)
+		{
+			sum = sum + j
+		}
+		next = j + k;
+		j = k;
+		k = next;
+	}
+	printf("%lu\n", sum);
+
+	return (0);
 }
