4a126c7f706db8e3e67302125d253ef12aca11dc
phemsie <femi_oworu@yahoo.com>
2022-10-10 16:57:24 -0700

    Task 0

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -6,25 +6,17 @@
  * @name: name of dog
  * @age: age of dog
  * @owner: owner of dog
- *
  */
 
-struct dog;
+struct dog
 {
 	char *name;
 	float age;
 	char *owner;
-}
-
-/**
- * dog_t - Typedef for struct dog
- */
-
+};
 typedef struct dog dog_t;
-
 void init_dog(struct dog *, char *, float, char *);
 void print_dog(struct dog *);
 dog_t *new_dog(char *, float, char *);
 void free_dog(dog_t *);
-
 #endif
