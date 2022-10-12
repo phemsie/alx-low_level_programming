cd07b1bca898b02303bc19d67e5ba25274acd136
phemsie <femi_oworu@yahoo.com>
2022-10-12 16:41:44 -0700

    Task 3

---
+++ 0x0F-function_pointers/3-get_op_func.c
@@ -0,0 +1,31 @@
+#include "calc.h"
+#include <string.h>
+
+/**
+ * get_op_func - select the current operation to perform
+ * @s: operation to perform
+ *
+ * Return: pointer to correct function
+ */
+
+int (*get_op_func(char *s))(int, int)
+{
+	op_t ops[] = {
+		{"+", op_add},
+		{"-", op_sub},
+		{"*", op_mul},
+		{"/", op_div},
+		{"%", op_mod},
+		{NULL, NULL}
+	};
+	int i;
+
+	i = 0;
+	while (ops[i].op != NULL)
+	{
+		if (strcmp(s, ops[i].op) == 0)
+			break;
+		i++;
+	}
+	return (ops[i].f);
+}
