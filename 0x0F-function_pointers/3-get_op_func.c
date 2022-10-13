a465ca1bc5b0f52e587b2bd784e4da72720c2f6c
phemsie <femi_oworu@yahoo.com>
2022-10-12 18:58:16 -0700

    3-main.c, 3-op_functions.c, 3-get_op_func.c, 3-calc.h

--- 0x0F-function_pointers/3-get_op_func.c
+++ 0x0F-function_pointers/3-get_op_func.c
@@ -22,11 +22,11 @@ int (*get_op_func(char *s))(int, int)
 
 	i = 0;
 
-	while (ops[i].op)
+	while (ops[i].op != NULL)
 	{
 		if (strcmp(ops[i].op, s) == 0)
-			return (ops[i].f);
+			break;
 		i++;
 	}
-	return (NULL);
+	return (ops[i].f);
 }
