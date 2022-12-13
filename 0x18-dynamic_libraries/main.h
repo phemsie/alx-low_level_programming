d2259a710b6b8db15b1f8455097bb5b9f65fd4ff
phemsie <femi_oworu@yahoo.com>
2022-12-13 04:56:45 -0800

    dynamic_libraries

--- 0x18-dynamic_libraries/main.h
+++ 0x18-dynamic_libraries/main.h
@@ -1,14 +1,18 @@
 #ifndef MAIN_H
 #define MAIN_H
 
+#include <stdio.h>
+#include <stdlib.h>
+
 int _putchar(char c);
 int _islower(int c);
 int _isalpha(int c);
-int _abs(int n);
+int _abs(int);
 int _isupper(int c);
 int _isdigit(int c);
 int _strlen(char *s);
-void _puts(char *s)char *_strcpy(char *dest, char *src);
+void _puts(char *str);
+char *_strcpy(char *dest, char *src);
 int _atoi(char *s);
 char *_strcat(char *dest, char *src);
 char *_strncat(char *dest, char *src, int n);
