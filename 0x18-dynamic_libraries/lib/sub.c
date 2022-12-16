a42f7c736b3d3aa7928de597f13ace4ddb0aee71
phemsie <femi_oworu@yahoo.com>
2022-12-16 06:31:19 -0800

    dynamic_libraries

---
+++ 0x18-dynamic_libraries/lib/sub.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * sub - subtracts two integers
+ * @a: integer to be subtracted
+ * @b: integer to be subtracted
+ * Returns: returns the subtraction of a and b
+ */
+
+int sub(int a, int b)
+{
+	return (a - b);
+}
